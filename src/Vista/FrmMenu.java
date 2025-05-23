45b2080277845e5ffa6ee8249ab15cb09c059fb3
Luis Eduardo Aldazaba Morales <lalomorales2141@outlook.com>
2025-05-23 17:45:24 -0600

    se agrego gestion clientes hasta 11

--- src/Vista/FrmMenu.java
+++ src/Vista/FrmMenu.java
@@ -147,6 +147,11 @@ public class FrmMenu extends javax.swing.JFrame {
         jMenuItem_gestionar_cliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMG/cliente.png"))); // NOI18N
         jMenuItem_gestionar_cliente.setText("Gestionar Cliente");
         jMenuItem_gestionar_cliente.setPreferredSize(new java.awt.Dimension(180, 30));
+        jMenuItem_gestionar_cliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem_gestionar_clienteActionPerformed(evt);
+            }
+        });
         jMenu3.add(jMenuItem_gestionar_cliente);
 
         jMenuBar1.add(jMenu3);
@@ -316,6 +321,12 @@ public class FrmMenu extends javax.swing.JFrame {
         interCliente.setVisible(true);
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem_nuevo_clienteActionPerformed
+
+    private void jMenuItem_gestionar_clienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem_gestionar_clienteActionPerformed
+        InterGestionarCliente interGestionarCliente = new InterGestionarCliente();
+        jDesktopPane_menu.add(interGestionarCliente);
+        interGestionarCliente.setVisible(true);
+    }//GEN-LAST:event_jMenuItem_gestionar_clienteActionPerformed
 
     /**
      * @param args the command line arguments
